a5174691c9a1df4a85bf20bb1b96aad0f47c8dcc
Luann Jung <luju@mit.edu>
2021-11-07 19:10:12 +0000

    udpated legends

--- simpledata/clustering.py
+++ simpledata/clustering.py
@@ -14,9 +14,12 @@ eps_options = [0.05, 0.1, 0.25, 0.5, 1]
 for ep in tqdm(eps_options):
     dbscan = cluster.DBSCAN(eps=ep, min_samples=5)
     clustered = dbscan.fit_predict(grads)
+    num_clusters = np.unique(clustered).size-1
     fig = plt.figure()
     ax = Axes3D(fig)
     scattered = ax.scatter(grads[:,0], grads[:,1], grads[:,2], c=clustered, cmap="Spectral")
-    legend = ax.legend(scattered.legend_elements(), loc="lower left", title="Cluster")
-    ax.add_artist(legend)
+    ax.set_xlabel('Gradient wrt weight 0')
+    ax.set_ylabel('Gradient wrt weight 1')
+    ax.set_zlabel('Gradient wrt bias')
+    ax.text2D(0.95, 0.05, str(num_clusters) + " clusters + outliers", transform=ax.transAxes)
     plt.savefig("cluster_ep_" + str(ep) + ".pdf")
