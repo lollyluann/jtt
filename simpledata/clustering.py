3bc3b28dc40de322312412fa50578c85c6b835d8
Luann Jung <luju@mit.edu>
2021-11-07 18:46:55 +0000

    legends upda ted

--- simpledata/clustering.py
+++ simpledata/clustering.py
@@ -9,12 +9,14 @@ data_dir = "weight_bias_grads.npy"
 grads = np.load(data_dir)
 print("Loaded gradients of shape", grads.shape)
 
-eps_options = [0.05, 0.1, 0.5, 1]
+eps_options = [0.05, 0.1, 0.25, 0.5, 1]
 
 for ep in tqdm(eps_options):
     dbscan = cluster.DBSCAN(eps=ep, min_samples=5)
     clustered = dbscan.fit_predict(grads)
     fig = plt.figure()
     ax = Axes3D(fig)
-    ax.scatter(grads[:,0], grads[:,1], grads[:,2], c=clustered, cmap="Spectral")
+    scattered = ax.scatter(grads[:,0], grads[:,1], grads[:,2], c=clustered, cmap="Spectral")
+    legend = ax.legend(scattered.legend_elements(), loc="lower left", title="Cluster")
+    ax.add_artist(legend)
     plt.savefig("cluster_ep_" + str(ep) + ".pdf")
