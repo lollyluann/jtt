9a9b34d9731c616d262f42b0dfa98017925a253a
Luann Jung <luju@mit.edu>
2021-11-27 17:48:49 +0000

    waterbird grad test

--- resnet50_test.py
+++ resnet50_test.py
@@ -2,27 +2,37 @@ import torch
 import torchvision
 import wandb
 import torch.nn as nn
+import numpy as np
 
 from utils import hinge_loss
 from train import train
+from data.confounder_utils import prepare_confounder_data
+from data import dro_dataset
+from tqdm import tqdm
 
-pretrained = false
-n_classes = 2
+device = torch.device("cuda")
+
+def full_detach(x):
+    return x.squeeze().detach().cpu().numpy()
+
+pretrained = True
+#n_classes = 2
 
 model = torchvision.models.resnet50(pretrained=pretrained)
-d = model.fc.in_features
-model.fc = nn.Linear(d, n_classes)
+#d = model.fc.in_features
+#model.fc = nn.Linear(d, n_classes)
+model = model.to(device)
 
-use_wandb = True
+use_wandb = False
 if use_wandb:
     wandb.watch(model)
 
-logger.flush()
+#logger.flush()
 
 criterion = hinge_loss
 epoch_offset = 0
 
-train(model,
+'''train(model,
       criterion,
       data,
       logger,
@@ -33,5 +43,63 @@ train(model,
       epoch_offset=epoch_offset,
       csv_name=fold,
       wandb=wandb if use_wandb else None)
+'''
 
+class DefaultArgs:
+    def __init__(self):
+        self.root_dir = "./cub"
+        self.dataset = "CUB"
+        self.target_name = "waterbird_complete95"
+        self.confounder_names = ["forest2water2"]
+        self.model = "resnet50"
+        self.metadata_csv_name = None
+        self.augment_data = False
+        self.fraction = 1.0
 
+def get_data():
+    loader_kwargs = {"batch_size": 64,
+                     "num_workers": 4,
+                     "pin_memory": True}
+    args = DefaultArgs()
+
+    train_data, val_data, test_data = prepare_confounder_data(args, train=True, return_full_dataset=False)
+
+    train_loader = dro_dataset.get_loader(train_data,
+                                          train=True,
+                                          reweight_groups=None,
+                                          **loader_kwargs)
+
+    val_loader = dro_dataset.get_loader(val_data,
+                                        train=False,
+                                        reweight_groups=None,
+                                        **loader_kwargs)
+    
+    test_loader = dro_dataset.get_loader(test_data,
+                                         train=False,
+                                         reweight_groups=None,
+                                         **loader_kwargs)
+    
+    all_embed = []
+    all_y = []
+    for idx, batch in enumerate(tqdm(train_loader)):
+        batch = tuple(t.to(device) for t in batch)
+        x = batch[0]
+        y = batch[1]
+        g = batch[2]
+        data_idx = batch[3]
+
+        outputs = model(x)
+
+        if idx == 0:
+            all_embed = full_detach(outputs)
+            all_y = full_detach(y)
+        else:
+            all_embed = np.concatenate([all_embed, full_detach(outputs)], axis=0)
+            all_y = np.concatenate([all_y, full_detach(y)])
+
+    print(all_embed.shape)
+    print(all_y.shape)
+    np.save("train_data_resnet50.npy", all_embed)
+    np.save("train_data_y_resnet50.npy", all_y)
+
+get_data()
---
+++ waterbirds_grads/simulate_grad.py
@@ -0,0 +1,136 @@
+import torch, math
+from tqdm import tqdm
+import numpy as np
+from utils import simul_x_y_a, add_outliers, plot_sample, plot_decision, plot_grad, plot_3d
+from metrics import group_metrics
+
+device = torch.device("cuda")
+
+def full_detach(x):
+    return x.squeeze().detach().cpu().numpy()
+'''
+mu_mult = 2.
+cov_mult = 1.
+skew = 5.
+
+train_prop_mtx = [[0.4, 0.1],[0.4, 0.1]]
+train_x, train_a, train_y = simul_x_y_a(train_prop_mtx, n=1000, mu_mult=mu_mult, 
+                                        cov_mult=cov_mult, skew=skew, outliers=True)
+plot_sample(train_x, train_a, train_y, title='Train')
+
+test_prop_mtx = [[0.25, 0.25],[0.25, 0.25]]
+test_x, test_a, test_y = simul_x_y_a(test_prop_mtx, n=1000, mu_mult=mu_mult, 
+                                     cov_mult=cov_mult, skew=skew, outliers=False)
+plot_sample(test_x, test_a, test_y, title='Test')
+
+train_x, train_y, text_x, test_y = map(torch.tensor, (train_x, train_y, test_x, test_y))
+train_x.requires_grad_()
+test_x = torch.tensor(test_x)
+test_x.requires_grad_()
+train_y.double()
+
+test_biased_x, test_biased_a, test_biased_y = simul_x_y_a(train_prop_mtx, n=1000, 
+                                                          mu_mult=mu_mult, cov_mult=cov_mult,
+                                                          skew=skew)
+
+'''
+
+train_x = torch.tensor(np.load("../train_data_resnet50.npy"), dtype=torch.float64, device=device, requires_grad=True)
+train_y = torch.tensor(np.load("../train_data_y_resnet50.npy"), device=device)
+
+
+a = train_x.shape[1]
+weights = torch.randn(a, 1, device=device, dtype=torch.double)/math.sqrt(a)
+weights.requires_grad_()
+bias = torch.zeros(1, requires_grad=True, device=device, dtype=torch.double)
+
+def model(xb):
+    sig = torch.nn.Sigmoid().to(device)
+    return sig(xb @ weights + bias)
+
+loss_func = torch.nn.BCELoss()
+lr = 0.05
+epochs = 100
+
+weight_traingrad, input_traingrad, bias_traingrad = [], [], []
+weight_testgrad, input_testgrad, bias_testgrad = [], [], []
+
+for epoch in tqdm(range(epochs)):
+    pred = model(train_x).squeeze()
+    loss = loss_func(pred, train_y.double())
+    loss.backward()
+    input_traingrad = full_detach(train_x.grad)
+   
+    with torch.no_grad():
+        weights -= weights.grad * lr
+        bias -= bias.grad * lr
+        weights.grad.zero_()
+        bias.grad.zero_()
+    
+    if epoch==epochs-1:
+        weight_traingrad, bias_traingrad = [], []
+        for i, pt in enumerate(train_x):
+            ptpred = torch.squeeze(model(pt[None, :]), dim=1)
+            loss2 = loss_func(ptpred, train_y[i:i+1].double())
+            loss2.backward()
+            weight_grad = full_detach(weights.grad)
+            bias_grad = full_detach(bias.grad)
+            weight_traingrad.append(weight_grad.copy())
+            bias_traingrad.append(bias_grad.copy())
+            with torch.no_grad():
+                weights.grad.zero_()
+                bias.grad.zero_()
+    
+    '''
+    testloss = loss_func(model(test_x), test_y)
+    testloss.backward()
+    input_testgrad = test_x.grad.squeeze().detach().cpu().numpy()
+    weight_testgrad = weights.grad.squeeze().detach().cpu().numpy()
+    bias_testgrad = bias.grad.squeeze().detach().cpu().numpy()
+    weights.grad.zero_()
+    bias.grad.zero_()'''
+
+
+def predict(model, data):
+    if not isinstance(data, torch.Tensor):
+        data = torch.tensor(data)
+    return full_detach(model(data).round())
+
+## Base classifier
+#base_predict = predict(model, train_x)
+#print('Baseline')
+#_ = group_metrics(full_detach(train_y), base_predict, test_a, label_protected=1, label_good=0)
+#print('Test biased accuracy', np.mean(predict(model, test_biased_x) == test_biased_y))
+
+## Base ideal classifier
+'''base_lr_ideal = LogisticRegression(solver='liblinear', fit_intercept=True)
+base_lr_ideal.fit(test_x, test_y)
+base_predict_ideal = base_lr_ideal.predict(test_x)
+print('\nBaseline IDEAL')
+_ = group_metrics(test_y, base_predict_ideal, test_a, label_protected=1, label_good=0)
+'''
+#plot_decision(full_detach(test_x), test_a, full_detach(test_y), lambda x: predict(model, x), title='Log Reg')
+#plot_decision(test_x, test_a, test_y, lambda x: base_lr_ideal.predict_proba(x)[:,1], title='Log Reg IDEAL')
+
+weight_traingrad = np.array(weight_traingrad)
+bias_traingrad = np.array(bias_traingrad)
+print(weight_traingrad.shape, input_traingrad.shape, bias_traingrad)
+print(weight_traingrad.sum(), bias_traingrad.sum())
+#plot_grad(full_detach(train_x), train_a, full_detach(train_y), input_traingrad, title="TrainGradInput")
+#plot_grad(full_detach(train_x), train_a, full_detach(train_y), weight_traingrad, title="TrainGradWeight")
+#plot_3d(full_detach(train_x), full_detach(train_y), train_a, weight_traingrad, bias_traingrad, title="TrainGradWeightsBias")
+#plot_grad(full_detach(train_x), train_a, full_detach(train_y), bias_traingrad, title="TrainGradBias")
+#plot_grad(full_detach(test_x), test_a, full_detach(test_y), input_testgrad, title="TestGrad")
+
+grads = np.append(weight_traingrad, bias_traingrad[np.newaxis].T, axis=1)
+print(weight_traingrad.shape, bias_traingrad.shape, grads.shape)
+save_dir = "weight_bias_grads.npy"
+np.save(save_dir, grads)
+
+
+
+
+
+
+
+
