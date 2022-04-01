2aee29214dd82ab3f0fe5d21bb8778eff40eeb31
Luann Jung <luju@mit.edu>
2022-04-01 15:38:53 +0000

    updated code

--- data/dro_dataset.py
+++ data/dro_dataset.py
@@ -47,6 +47,12 @@ class DRODataset(Dataset):
         else:
             raise NotImplementedError
 
+    def get_label_array(self):
+        if self.process_item is None:
+            return self.dataset.get_label_array()
+        else:
+            raise NotImplementedError
+    
     def __len__(self):
         return len(self.dataset)
 
